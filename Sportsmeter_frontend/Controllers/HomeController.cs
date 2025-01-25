445b312aaa67d8b019781d6ba94d69fdad5921a3
amrali21 <amoormoneer@gmail.com>
2025-01-25 20:13:02 +0200

    make separate controller for reports

--- Sportsmeter_frontend/Controllers/HomeController.cs
+++ Sportsmeter_frontend/Controllers/HomeController.cs
@@ -151,50 +151,6 @@ namespace Sportsmeter_frontend.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        public async Task<IActionResult> Report()
-        {
-            List<RunInfo> all = await _runInfoRepository.GetAllAsync(); // data needs to come filtered by user and select only relevant columns
-            DateTime mostRecent = all.Select(r => r.Date).Max();
-            DateTime oldest = all.Select(all => all.Date).Min();
-
-            DateTime tempDate = mostRecent;  // 21/01/2025
-
-            List<WeeklyReport> weeklyReports = new();
-            while(tempDate > oldest.AddDays(-7))         //ignore: check condition for when there is like 5 day difference
-            {                                            
-                // get avg of this week
-                DateTime dateTo = tempDate;               //ignore: 1) 21/01/2025      2) 13/01/2025 (this needs to be 13 instead)       
-                DateTime dateFrom = tempDate.AddDays(-7); //ignore  1) 14/01/2025      2) 07/01/2025
-                tempDate = dateFrom.AddDays(-1); // ignore: is there a way to avoid this?
-
-                float avgDist = 0;
-                float avgTime = 0;
-
-                Func<RunInfo, bool> query = r => r.Date >= dateFrom && r.Date <= dateTo && r.ApplicationUserId == UserID.Value.ToString();
-                
-                if (!all.Any(query))
-                    continue;
-
-                avgDist = all.Where(query)
-                    .Select(r => r.Time).Average();
-
-                avgTime = all.Where(query)
-                    .Select(r => r.Distance).Average();
-
-
-                weeklyReports.Add(
-                    new WeeklyReport 
-                    { 
-                        from = dateTo,
-                        to = dateFrom,
-                        avgDist = avgDist,
-                        avgTime = avgTime
-                    });
-            }
-
-            return View(weeklyReports);
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Sportsmeter_frontend/Controllers/ReportController.cs
@@ -0,0 +1,67 @@
+﻿using CRUD_Design.Contracts;
+using CRUD_Design.Models.DBModel;
+using CRUD_Design.Models.DTO.Reports;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Sportsmeter_frontend.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly IRunInfoRepository _runInfoRepository;
+        private Claim? UserID => User.Claims.FirstOrDefault(c => c.Type == "uid");
+
+        public ReportController(IRunInfoRepository runInfoRepository) {
+            _runInfoRepository = runInfoRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("Weekly");
+        }
+
+        public async Task<IActionResult> Weekly()
+        {
+            List<RunInfo> all = await _runInfoRepository.GetAllAsync(); // data needs to come filtered by user and select only relevant columns
+            DateTime mostRecent = all.Select(r => r.Date).Max();
+            DateTime oldest = all.Select(all => all.Date).Min();
+
+            DateTime tempDate = mostRecent;  // 21/01/2025
+
+            List<WeeklyReport> weeklyReports = new();
+            while (tempDate > oldest.AddDays(-7))         //ignore: check condition for when there is like 5 day difference
+            {
+                // get avg of this week
+                DateTime dateTo = tempDate;               //ignore: 1) 21/01/2025      2) 13/01/2025 (this needs to be 13 instead)       
+                DateTime dateFrom = tempDate.AddDays(-7); //ignore  1) 14/01/2025      2) 07/01/2025
+                tempDate = dateFrom.AddDays(-1); // ignore: is there a way to avoid this?
+
+                float avgDist = 0;
+                float avgTime = 0;
+
+                Func<RunInfo, bool> query = r => r.Date >= dateFrom && r.Date <= dateTo && r.ApplicationUserId == UserID.Value.ToString();
+
+                if (!all.Any(query))
+                    continue;
+
+                avgDist = all.Where(query)
+                    .Select(r => r.Time).Average();
+
+                avgTime = all.Where(query)
+                    .Select(r => r.Distance).Average();
+
+
+                weeklyReports.Add(
+                    new WeeklyReport
+                    {
+                        from = dateTo,
+                        to = dateFrom,
+                        avgDist = avgDist,
+                        avgTime = avgTime
+                    });
+            }
+
+            return View("Index", weeklyReports);
+        }
+    }
+}
